b6c48c538b58466be238f27da215190ce063e750
Daniil Chupin <danielchupin@yandex.ru>
2023-07-21 13:56:08 +0700

    HW is done - 1.0 ver

--- ColorizedApp/ContentView.swift
+++ ColorizedApp/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redValue: Double = 0.5
+    @State private var greenValue: Double = 0.5
+    @State private var blueValue: Double = 0.5
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ColorPreviewView(redValue: redValue, greenValue: greenValue, blueValue: blueValue)
+            
+            ColorSlider(value: $redValue, color: .red)
+            ColorSlider(value: $greenValue, color: .green)
+            ColorSlider(value: $blueValue, color: .blue)
         }
         .padding()
     }
@@ -22,5 +27,43 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorPreviewView: View {
+    let redValue: Double
+    let greenValue: Double
+    let blueValue: Double
+    
+    var body: some View {
+        Color(red: redValue, green: greenValue, blue: blueValue)
+            .frame(height: 200)
+            .cornerRadius(10)
+            .padding(.bottom, 30)
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var color: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(colorDescription(for: color)): \(value * 255.0, specifier: "%.0f")")
+                .frame(width: 100, alignment: .leading)
+            
+            Slider(value: $value, in: 0...1, step: 0.01)
+                .accentColor(color)
+        }
+        .padding()
+    }
+    
+    private func colorDescription(for color: Color) -> String {
+        switch color {
+        case .red: return "Red"
+        case .green: return "Green"
+        case .blue: return "Blue"
+        default: return ""
+        }
     }
 }
